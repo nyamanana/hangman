4e562fda7708e976356fc000b100303b930d432a
nyamanana <nyama409@gmail.com>
2018-12-11 14:54:52 +0900

    my first commit

---
+++ 12-1.py
@@ -0,0 +1,7 @@
+class Apple:
+    def  __init__(self,num,lar,col,wei):
+        self.number = num
+        self.large = lar
+        self.color = col
+        self.weight = wei
+        print("Created!")
